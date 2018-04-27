2813e961251281bed2d38067a86b0d3a845a0600
Stefan Hecken <stefan.hecken@concepts-and-training.de>
2018-04-27 11:29:43 +0200

    1013 refactor tests for link helper

--- tests/TMS/CourseCreation/LinkHelperTest.php
+++ tests/TMS/CourseCreation/LinkHelperTest.php
@@ -49,7 +49,7 @@ class LinkHelperTest extends TestCase {
 		}
 	}
 
-	public function test__noOpenRequests() {
+	public function test_user_has_no_open_request() {
 		$usr = $this->getMockBuilder(ilObjUser::class)
 			->disableOriginalConstructor()
 			->getMock();
@@ -69,20 +69,11 @@ class LinkHelperTest extends TestCase {
 			->method("getUser")
 			->will($this->returnValue($usr));
 
-		$this->assertNull($link_helper->_maybeShowRequestInfo());
+		$this->assertFalse($link_helper->_maybeShowRequestInfo());
 	}
 
-	public function test_openRequests() {
+	public function test_user_has_open_requests() {
 		$txt_message = "This is the user info";
-
-		$lng = $this->getMockBuilder(ilLanguage::class)
-			->setMethods(array("txt"))
-			->disableOriginalConstructor()
-			->getMock();
-
-		$lng->expects($this->once())
-			->method("txt")
-			->will($this->returnValue($txt_message));
 
 		$usr = $this->getMockBuilder(ilObjUser::class)
 			->disableOriginalConstructor()
@@ -94,8 +85,7 @@ class LinkHelperTest extends TestCase {
 					"getUsersDueRequests"
 					, "getUser"
 					, "sendInfo"
-					, "getTrainingTitleByRequest"
-					, "getLng"
+					, "getMessage"
 				)
 			)
 			->getMock();
@@ -109,15 +99,12 @@ class LinkHelperTest extends TestCase {
 			->will($this->returnValue(array($request)));
 
 		$link_helper->expects($this->once())
-			->method("getTrainingTitleByRequest");
+			->method("getMessage")
+			->will($this->returnValue($txt_message));
 
 		$link_helper->expects($this->once())
 			->method("sendInfo")
 			->with($this->equalTo($txt_message));
-
-		$link_helper->expects($this->once())
-			->method("getLng")
-			->will($this->returnValue($lng));
 
 		$link_helper->expects($this->once())
 			->method("getUser")
@@ -126,189 +113,59 @@ class LinkHelperTest extends TestCase {
 		$this->assertTrue($link_helper->_maybeShowRequestInfo());
 	}
 
-	public function test_openRequestsWithPlugin() {
-		if(class_exists("ilCourseCreationPlugin")) {
-
-			$xccr_plugin = $this->getMockBuilder(ilCourseCreationPlugin::class)
-				->setMethods(array("getActions"))
-				->disableOriginalConstructor()
-				->getMock();
-
-			$txt_message = "This is the user info";
-			$lng = $this->getMockBuilder(ilLanguage::class)
-				->setMethods(array("txt"))
-				->disableOriginalConstructor()
-				->getMock();
-
-			$lng->expects($this->once())
-				->method("txt")
-				->will($this->returnValue($txt_message));
-
-			$usr = $this->getMockBuilder(ilObjUser::class)
-				->disableOriginalConstructor()
-				->getMock();
-
-			$link_helper = $this->getMockBuilder(LinkHelperMock::class)
-				->setMethods(
-					array(
-						"getUsersDueRequests"
-						, "getUser"
-						, "sendInfo"
-						, "getTrainingTitleByRequest"
-						, "getLng"
-					)
-				)
-				->getMock();
-
-			$request = $this->getMockBuilder(\ILIAS\TMS\CourseCreation\Request::class)
-				->disableOriginalConstructor()
-				->getMock();
-
-			$link_helper->expects($this->once())
-				->method("getUsersDueRequests")
-				->will($this->returnValue(array($request)));
-
-			$link_helper->expects($this->once())
-				->method("getTrainingTitleByRequest");
-
-			$link_helper->expects($this->once())
-				->method("sendInfo")
-				->with($this->equalTo($txt_message));
-
-			$link_helper->expects($this->once())
-				->method("getLng")
-				->will($this->returnValue($lng));
-
-			$link_helper->expects($this->once())
-				->method("getUser")
-				->will($this->returnValue($usr));
-
-			$this->assertTrue($link_helper->_maybeShowRequestInfo($xccr_plugin));
-		}
-	}
-
-	public function test_noUsersDueRequestsBecauseOfNoPlugin() {
+	public function test_user_has_open_cached_requests() {
 		$usr = $this->getMockBuilder(ilObjUser::class)
-			->setMethods(array("getId"))
 			->disableOriginalConstructor()
 			->getMock();
 
-		$usr->expects($this->once())
-			->method("getId")
-			->will($this->returnValue(2));
+		$link_helper = $this->getMockBuilder(LinkHelperMock::class)
+			->setMethods(
+				array(
+					"getCachedRequests"
+					, "setCachedRequests"
+				)
+			)
+			->getMock();
+
+		$request = $this->getMockBuilder(\ILIAS\TMS\CourseCreation\Request::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$link_helper->expects($this->once())
+			->method("getCachedRequests")
+			->will($this->returnValue(array($request)));
+
+		$link_helper->expects($this->never())
+			->method("setCachedRequests");
+
+		$this->assertEquals(array($request), $link_helper->_getUsersDueRequests($usr));
+	}
+
+	public function test_user_has_no_cached_request_and_no_plugin() {
+		$usr = $this->getMockBuilder(ilObjUser::class)
+			->disableOriginalConstructor()
+			->getMock();
 
 		$link_helper = $this->getMockBuilder(LinkHelperMock::class)
-			->setMethods(array("getCachedRequests", "setCachedRequests"))
+			->setMethods(
+				array(
+					"getCachedRequests"
+					, "setCachedRequests"
+				)
+			)
+			->getMock();
+
+		$request = $this->getMockBuilder(\ILIAS\TMS\CourseCreation\Request::class)
+			->disableOriginalConstructor()
 			->getMock();
 
 		$link_helper->expects($this->once())
 			->method("getCachedRequests")
 			->will($this->returnValue(null));
 
-		$this->assertEquals(array(), $link_helper->_getUsersDueRequests($usr));
-	}
-
-	public function test_usersDueRequestsCached() {
-		$usr = $this->getMockBuilder(ilObjUser::class)
-			->setMethods(array("getId"))
-			->disableOriginalConstructor()
-			->getMock();
-
-		$usr->expects($this->once())
-			->method("getId")
-			->will($this->returnValue(2));
-
-		$link_helper = $this->getMockBuilder(LinkHelperMock::class)
-			->setMethods(array("getCachedRequests", "setCachedRequests"))
-			->getMock();
-
-		$link_helper->expects($this->once())
-			->method("getCachedRequests")
-			->will($this->returnValue(array()));
+		$link_helper->expects($this->never())
+			->method("setCachedRequests");
 
 		$this->assertEquals(array(), $link_helper->_getUsersDueRequests($usr));
-	}
-
-	public function test_usersDueRequestsFromPlugin() {
-		if(class_exists("ilCourseCreationPlugin")) {
-			$xccr_plugin = $this->getMockBuilder(ilCourseCreationPlugin::class)
-				->setMethods(array("getActions"))
-				->disableOriginalConstructor()
-				->getMock();
-
-			$actions = $this->getMockBuilder(ilActions::class)
-				->setMethods(array("getDueRequestsOf"))
-				->disableOriginalConstructor()
-				->getMock();
-
-			$actions->expects($this->once())
-				->method("getDueRequestsOf")
-				->will($this->returnValue(array()));
-
-			$xccr_plugin->expects($this->once())
-				->method("getActions")
-				->will($this->returnValue($actions));
-
-			$usr = $this->getMockBuilder(ilObjUser::class)
-				->setMethods(array("getId"))
-				->disableOriginalConstructor()
-				->getMock();
-
-			$usr->expects($this->exactly(3))
-				->method("getId")
-				->will($this->returnValue(2));
-
-			$link_helper = $this->getMockBuilder(LinkHelperMock::class)
-				->setMethods(array("getCachedRequests", "setCachedRequests"))
-				->getMock();
-
-			$link_helper->expects($this->exactly(2))
-				->method("getCachedRequests")
-				->will($this->onConsecutiveCalls(null, array()));
-
-			$link_helper->expects($this->once())
-				->method("setCachedRequests")
-				->with(
-					$this->equalTo(2),
-					$this->equalTo(array())
-				);
-
-			$this->assertEquals(array(), $link_helper->_getUsersDueRequests($usr, $xccr_plugin));
-		}
-	}
-
-	public function test_getTrainingTitleByRequest() {
-		$request = $this->getMockBuilder(\ILIAS\TMS\CourseCreation\Request::class)
-			->setMethods(array("getConfigurations"))
-			->disableOriginalConstructor()
-			->getMock();
-
-		$request->expects($this->once())
-			->method("getConfigurations")
-			->will($this->returnValue(array(array(array("title" => "Course Title")))));
-
-		$link_helper = new LinkHelperMock();
-		$this->assertEquals("Course Title", $link_helper->_getTrainingTitleByRequest($request));
-	}
-
-	public function test_getTrainingTitleByRequestException() {
-		$request = $this->getMockBuilder(\ILIAS\TMS\CourseCreation\Request::class)
-			->setMethods(array("getConfigurations"))
-			->disableOriginalConstructor()
-			->getMock();
-
-		$request->expects($this->once())
-			->method("getConfigurations")
-			->will($this->returnValue(array()));
-
-		$link_helper = new LinkHelperMock();
-		$thrown = false;
-		try {
-			$title = $link_helper->_getTrainingTitleByRequest($request);
-		} catch(\RuntimeException $e) {
-			$thrown = true;
-		}
-
-		$this->assertTrue($thrown);
 	}
 }
