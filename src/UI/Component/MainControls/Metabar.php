9c82b2d0012e78f4335d84b6698169aaeab845eb
Christoph Krahl <christoph.krahl@concepts-and-training.de>
2018-11-30 09:13:46 +0100

    metabar deactivated getlogo-function

--- src/UI/Component/MainControls/Metabar.php
+++ src/UI/Component/MainControls/Metabar.php
@@ -3,30 +3,17 @@
 
 namespace ILIAS\UI\Component\MainControls;
 
-<<<<<<< HEAD
-use ILIAS\UI\Component as C;
-=======
 use ILIAS\UI\Component\Component;
 use ILIAS\UI\Component\Signal;
 use ILIAS\UI\Component\Image\Image;
->>>>>>> f3c273715418ee0afae500b77e4acb53a79d57fb
 use ILIAS\UI\Component\JavaScriptBindable;
 
 /**
  * This describes the Metabar.
  */
-<<<<<<< HEAD
-interface Metabar extends C\Component, JavaScriptBindable {
-
-	/**
-	 *  @return 	\ILIAS\UI\Component\Image\Image
-	 */
-	public function getLogo();
-=======
 interface Metabar extends Component, JavaScriptBindable
 {
-	public function getLogo(): Image;
->>>>>>> f3c273715418ee0afae500b77e4acb53a79d57fb
+	/* public function getLogo(): Image; */
 
 	/**
 	 * Append an entry.
