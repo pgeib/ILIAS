30d0dbca2a46087468a3ea824a7b9781a88c2198
Nils Haagen <nhaagen@cat06.de>
2019-01-07 13:13:45 +0100

    extended examples, fix icons for linux

--- src/UI/examples/MainControls/MainBar/mainbar.php
+++ src/UI/examples/MainControls/MainBar/mainbar.php
@@ -4,11 +4,11 @@ function mainbar()
 	global $DIC;
 	$f = $DIC->ui()->factory();
 	$renderer = $DIC->ui()->renderer();
-	$mainbar = buildMainbar($f);
+	$mainbar = buildMainbar($f, $renderer);
 	return $renderer->render($mainbar);
 }
 
-function buildMainbar($f)
+function buildMainbar($f, $r)
 {
 	$tools_btn = $f->button()->bulky(
 		$f->icon()->custom('./src/UI/examples/Layout/Page/Standard/grid.svg', ''),
@@ -18,10 +18,22 @@ function buildMainbar($f)
 	$mainbar = $f->mainControls()->mainbar()
 		->withToolsButton($tools_btn);
 
-	list($entries, $tools) = getSomeEntries($f);
+
+	$entries = [];
+
+
+	$entries['repository'] = getDemoEntryRepository($f);
+	$entries['pws'] = getDemoEntryPersonalWorkspace($f, $r);
+	$entries['achievements'] = getDemoEntryAchievements($f);
+	$entries['communication'] = getDemoEntryCommunication($f);
+	$entries['organisation'] = getDemoEntryOrganisation($f);
+	$entries['administration'] = getDemoEntryAdministration($f);
+
+	//list($entries, $tools) = getDemoEntries($f);
 	foreach ($entries as $id=>$entry) {
 		$mainbar = $mainbar->withAdditionalEntry($id, $entry);
 	}
+	$tools = getDemoEntryTools($f);
 	foreach ($tools as $id=>$entry) {
 		$mainbar = $mainbar->withAdditionalToolEntry($id, $entry);
 	}
@@ -29,55 +41,100 @@ function buildMainbar($f)
 	return $mainbar;
 }
 
-function getSomeEntries($f)
+function getDemoEntryRepository($f)
 {
-	$entries = [];
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/layers.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Repository', $symbol, '');
+
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('X');
+	$button = $f->button()->bulky($icon, 'Button 1', './src/UI/examples/Layout/Page/Standard/ui.php?new_ui=1');
+	$slate = $slate
+		->withAdditionalEntry($button->withLabel('Repository - Home'))
+		->withAdditionalEntry($button->withLabel('Repository - Tree'))
+		->withAdditionalEntry($button->withLabel('Repository - Last visited'))
+		->withAdditionalEntry($button->withLabel('Favorites'))
+		->withAdditionalEntry($button->withLabel('Courses'))
+		->withAdditionalEntry($button->withLabel('Groups'))
+		->withAdditionalEntry($button->withLabel('Study Programme'))
+		->withAdditionalEntry($button->withLabel('Own Repository-Objects'))
+		;
+	return $slate;
+}
+
+function getDemoEntryPersonalWorkspace($f, $r)
+{
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('X');
+	$button = $f->button()->bulky($icon, 'Button 1', './src/UI/examples/Layout/Page/Standard/ui.php?new_ui=1');
+
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/user.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Personal Workspace', $symbol, '');
+
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/bookmarks.svg', '')->withSize('small');
+	$bookmarks = implode('<br />', [
+		$r->render($f->button()->shy('my bookmark 1', '#')),
+		$r->render($f->button()->shy('my bookmark 2', '#'))
+	]);
+	$slate_bookmarks = $f->maincontrols()->slate()->legacy('Bookmarks', $symbol, $bookmarks);
+
+
+	$slate = $slate
+		->withAdditionalEntry($button->withLabel('Overview'))
+		->withAdditionalEntry($slate_bookmarks)
+		->withAdditionalEntry($button->withLabel('Calendar'))
+		->withAdditionalEntry($button->withLabel('Task'))
+		->withAdditionalEntry($button->withLabel('Portfolios'))
+		->withAdditionalEntry($button->withLabel('Personal Resources'))
+		->withAdditionalEntry($button->withLabel('Shared Resources'))
+		->withAdditionalEntry($button->withLabel('Notes'))
+		->withAdditionalEntry($button->withLabel('News'))
+		->withAdditionalEntry($button->withLabel('Background Tasks'))
+		;
+	return $slate;
+}
+
+function getDemoEntryAchievements($f)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/achievements.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Achievements', $symbol, '');
+	return $slate;
+}
+
+function getDemoEntryCommunication($f)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/communication.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Communication', $symbol, '');
+	return $slate;
+}
+
+function getDemoEntryOrganisation($f)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/organisation.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Organisation', $symbol, '');
+	return $slate;
+}
+
+function getDemoEntryAdministration($f)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Administration', $symbol, '');
+	return $slate;
+}
+
+function getDemoEntryTools($f)
+{
 	$tools = [];
 
-	//add a slate
-	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/layers.svg', '')->withSize('small');
-	$slate = $f->maincontrols()->slate()->legacy('Repository', $symbol, loremIpsum());
-	$entries['example1'] = $slate;
-
-	//a slate with buttons and more slates
-	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('X');
-	$button = $f->button()->bulky($icon, 'Button 1', '#');
-	$slate1 = $f->maincontrols()->slate()->legacy('Short Legacy', $symbol, '<h2>legacy content</h2>');
-	$slate2 = $f->maincontrols()->slate()->legacy('Long Legacy', $symbol, loremIpsum());
-	$slate3_1 = $f->maincontrols()->slate()->legacy('Legacy 3.1', $symbol, 'legacy content 3.1');
-	$slate3_2 = $f->maincontrols()->slate()->legacy('Legacy 3.2 - long', $symbol, loremIpsum(9));
-	$slate3 = $f->maincontrols()->slate()->combined('Slate with Sub-Slates', $symbol)
-		->withAdditionalEntry($slate3_1)
-		->withAdditionalEntry($slate3_2)
-		->withAdditionalEntry($button->withLabel('Button 3.3'));
-	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/user.svg', '')->withSize('small');
-	$slate = $f->maincontrols()->slate()->combined('Personal Desktop', $symbol)
-		->withAdditionalEntry($button)
-		->withAdditionalEntry($button->withLabel('Button 2'))
-		->withAdditionalEntry($button->withLabel('Button 3'))
-		->withAdditionalEntry($slate1)
-		->withAdditionalEntry($slate2)
-		->withAdditionalEntry($button->withLabel('Button 4'))
-		->withAdditionalEntry($slate3)
-		->withAdditionalEntry($button->withLabel('Button 5'))
-	;
-	$entries['example2'] = $slate;
-
-	//add a button
-	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/options.svg', '')->withSize('small');
-	$entries['extra'] = $f->button()->bulky($symbol,'More', '#');
-	//add tool(slate)
 	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/question.svg', '')->withSize('small');
-	$slate = $f->maincontrols()->slate()->legacy('Tool 1', $symbol, '<h2>tool 1</h2><p>Some Text for Tool 1 entry</p>');
+	$slate = $f->maincontrols()->slate()->legacy('Help', $symbol, '<h2>tool 1</h2><p>Some Text for Tool 1 entry</p>');
 	$tools['tool1'] = $slate;
 	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/pencil.svg', '')->withSize('small');
-	$slate = $f->maincontrols()->slate()->legacy('Tool 2', $symbol, '<h2>tool 2</h2><p>Some Text for Tool 1 entry</p>');
+	$slate = $f->maincontrols()->slate()->legacy('Editor', $symbol, '<h2>tool 2</h2><p>Some Text for Tool 1 entry</p>');
 	$tools['tool2'] = $slate;
 	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/notebook.svg', '')->withSize('small');
-	$slate = $f->maincontrols()->slate()->legacy('Tool 3', $symbol, loremIpsum());
+	$slate = $f->maincontrols()->slate()->legacy('Local Navigation', $symbol, loremIpsum());
 	$tools['tool3'] = $slate;
 
-	return [$entries, $tools];
+	return $tools;
 }
 
 function loremIpsum():string {
