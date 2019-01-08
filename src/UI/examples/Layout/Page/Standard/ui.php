0e3e886c03e1859dad1e917ca2e1912dd60a26e2
Nils Haagen <nhaagen@cat06.de>
2019-01-08 16:04:35 +0100

    extended and streamlined example

--- src/UI/examples/Layout/Page/Standard/ui.php
+++ src/UI/examples/Layout/Page/Standard/ui.php
@@ -18,20 +18,14 @@ if ($_GET['new_ui'] == '1') {
 	$f = $DIC->ui()->factory();
 	$renderer = $DIC->ui()->renderer();
 
-	$crumbs = array (
-		$f->link()->standard("entry1", '#'),
-		$f->link()->standard("entry2", '#'),
-		$f->link()->standard("entry3", '#'),
-		$f->link()->standard("entry4", '#')
-	);
-	$breadcrumbs = $f->breadcrumbs($crumbs);
-
 	$logo = $f->image()
 		->responsive("src/UI/examples/Image/HeaderIconLarge.svg", "ILIAS");
 
-	$content = pagedemoContent($f);
+	$breadcrumbs = demoBreadcrumbs($f);
 	$metabar = buildMetabar($f);
-	$mainbar = buildMainbar($f, $renderer);
+	$content = demoContent($f);
+	$mainbar = buildDemoMainbar($f, $renderer, $_GET);
+
 	if($_GET['mbactive']) {
 		$mainbar = $mainbar->withActive($_GET['mbactive']);
 	}
@@ -50,25 +44,241 @@ if ($_GET['new_ui'] == '1') {
 if ($_GET['slate_contents']) {
 	_initIliasForPreview();
 	$f = $DIC->ui()->factory();
-	$renderer = $DIC->ui()->renderer();
+	$r = $DIC->ui()->renderer();
 
-	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/user.svg', '')->withSize('small');
-	if($_GET['slate_contents'] == '1') {
-		$slate = $f->maincontrols()->slate()->legacy('Replaced', $symbol, 'This is replaced content.')
-			->withBacklink('Back', './src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=2');
-	}
-	if($_GET['slate_contents'] == '2') {
-		$slate = $f->maincontrols()->slate()->combined('Replace Content', $symbol, 'replace again');
-		$slate = $slate
-			->withAdditionalEntry(
-				$f->button()->bulky($symbol, 'Replace whole Slate', '#')
-				->withOnClick(
-					$slate->getReplaceContentSignal()
-						->withAsyncRenderUrl('./src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=1')
-				)
-			);
-	}
+	$slate = buildRPCedSlate($f, $r, $_GET['slate_contents']);
 
-	echo $renderer->renderAsync($slate);
+	echo $r->renderAsync($slate);
 	exit();
+}
+
+
+function buildDemoMainbar($f, $r, array $get)
+{
+	$tools_btn = $f->button()->bulky(
+		$f->icon()->custom('./src/UI/examples/Layout/Page/Standard/grid.svg', ''),
+		'Tools',
+		'#'
+	);
+	$mainbar = $f->mainControls()->mainbar()
+		->withToolsButton($tools_btn);
+
+	$entries = [
+		'Repository',
+		'Personal Workspace',
+		'Achievements',
+		'Communication',
+		'Organisation',
+		'Administration'
+	];
+
+	$tools = [
+		'Help',
+		'Editor',
+		'Local Navigation'
+	];
+
+	foreach ($entries as $id) {
+		$slate = getDemoEntry($f, $r, $id);
+		$mainbar = $mainbar->withAdditionalEntry($id, $slate);
+	}
+	foreach ($tools as $id) {
+		$slate = getDemoEntry($f, $r, $id);
+		$mainbar = $mainbar->withAdditionalToolEntry($id, $slate);
+	}
+	return $mainbar;
+}
+
+function getDemoEntry($f, $r, $which)
+{
+	$content = '';
+	switch ($which){
+		case 'Repository':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/layers.svg', '');
+			break;
+		case 'Personal Workspace':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/user.svg', '');
+			break;
+		case 'Achievements':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/achievements.svg', '');
+			$content = loremIpsum();
+			break;
+		case 'Communication':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/communication.svg', '');
+			$content = loremShort();
+			break;
+		case 'Organisation':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/organisation.svg', '');
+			$content = loremShort();
+			break;
+		case 'Administration':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '');
+			break;
+		case 'Help':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/question.svg', '');
+			$content = loremIpsum();
+			break;
+		case 'Editor':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/pencil.svg', '');
+			$content = loremShort();
+			break;
+		case 'Local Navigation':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/notebook.svg', '');
+			break;
+	}
+
+	$symbol = $symbol->withSize('small');
+
+	if(! $content) {
+		$slate = $f->maincontrols()->slate()->combined($which, $symbol);
+		$slate = populateEntries($f, $r, $slate);
+	} else {
+		$slate = $f->maincontrols()->slate()->legacy($which, $symbol, $content);
+	}
+
+	return $slate;
+}
+
+function populateEntries($f, $r, $slate)
+{
+	$entries = [];
+	if($slate->getName() === 'Repository') {
+		$entries = getRepositoryEntries($f);
+	}
+	if($slate->getName() === 'Personal Workspace') {
+		$entries = getPersonalWorkspaceEntries($f, $r);
+	}
+	if($slate->getName() === 'Administration') {
+		$entries = getAdministrationEntries($f, $r, $slate);
+	}
+
+	foreach ($entries as $entry) {
+		$slate = $slate->withAdditionalEntry($entry);
+	}
+	return $slate;
+
+}
+
+function buildRPCedSlate($f, $r, $which)
+{
+	switch ($which){
+		case 'General Settings':
+			return getGeneralSettingsSlate($f, $r);
+
+		case 'SomeSettings':
+			$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '')->withSize('small');
+			$slate = $f->maincontrols()->slate()->legacy('Some Settings', $symbol, loremShort())
+				->withBacklink('General Settings', './src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=General Settings');
+			return $slate;
+
+		case 'Layout':
+			return getLayoutSlate($f, $r)
+				->withBacklink('Administration', './src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=Administration');
+
+		case 'Administration':
+			return getDemoEntry($f, $r, 'Administration');
+	}
+}
+
+
+function getRepositoryEntries($f)
+{
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('X');
+	$button = $f->button()->bulky($icon, 'button', './src/UI/examples/Layout/Page/Standard/ui.php?new_ui=1&mbactive=Repository');
+	return [
+		$button->withLabel('Repository - Home'),
+		$button->withLabel('Repository - Tree'),
+		$button->withLabel('Repository - Last visited'),
+		$button->withLabel('Favorites'),
+		$button->withLabel('Courses'),
+		$button->withLabel('Groups'),
+		$button->withLabel('Study Programme'),
+		$button->withLabel('Own Repository-Objects')
+	];
+}
+
+function getPersonalWorkspaceEntries($f, $r)
+{
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('Y');
+	$button = $f->button()->bulky($icon, 'button', './src/UI/examples/Layout/Page/Standard/ui.php?new_ui=1&mbactive=Personal Workspace');
+	return [
+		$button->withLabel('Overview'),
+		getBookmarksSlate($f, $r),
+		$button->withLabel('Calendar'),
+		$button->withLabel('Tasks'),
+		$button->withLabel('Portfolios'),
+		$button->withLabel('Personal Resources'),
+		$button->withLabel('Shared Resources'),
+		$button->withLabel('Notes'),
+		$button->withLabel('News'),
+		$button->withLabel('Background Tasks')
+	];
+}
+
+function getBookmarksSlate($f, $r)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/bookmarks.svg', '')->withSize('small');
+	$bookmarks = implode('<br />', [
+		$r->render($f->button()->shy('my bookmark 1', '#')),
+		$r->render($f->button()->shy('my bookmark 2', '#'))
+	]);
+	$slate = $f->maincontrols()->slate()->legacy('Bookmarks', $symbol, $bookmarks)
+		->withEngaged(true);
+
+	return $slate;
+}
+
+
+function getAdministrationEntries($f, $r, $slate)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '')->withSize('small');
+
+	$general_settings = getGeneralSettingsSlate($f, $r);
+
+	$layout_settings = $f->button()->bulky($symbol, 'Layout and Styles', '')
+		->withOnClick(
+			$slate->getReplaceContentSignal()
+				->withAsyncRenderUrl('./src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=Layout')
+		);
+
+	return [
+		$general_settings,
+		$layout_settings
+	];
+}
+
+
+function getGeneralSettingsSlate($f, $r)
+{
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('General Settings', $symbol);
+
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('S');
+	$button =  $f->button()->bulky($icon, '', '')
+		->withOnClick(
+			$slate->getReplaceContentSignal()
+			->withAsyncRenderUrl('./src/UI/examples/Layout/Page/Standard/ui.php?slate_contents=SomeSettings')
+		);
+
+	$slate = $slate
+		->withAdditionalEntry($button->withLabel('BasicSetting'))
+		->withAdditionalEntry($button->withLabel('Server'))
+		->withAdditionalEntry($button->withLabel('CronJobs'))
+		;
+
+	return $slate;
+}
+
+
+function getLayoutSlate($f, $r)
+{
+	$icon = $f->icon()->standard('', '')->withSize('small')->withAbbreviation('S');
+	$symbol = $f->icon()->custom('./src/UI/examples/Layout/Page/Standard/administration.svg', '')->withSize('small');
+	$slate = $f->maincontrols()->slate()->combined('Layout', $symbol)
+		->withAdditionalEntry($f->maincontrols()->slate()->legacy('delos', $icon, loremIpsum()))
+		->withAdditionalEntry($f->maincontrols()->slate()->legacy('custom', $icon, loremIpsum()))
+		->withAdditionalEntry($f->maincontrols()->slate()->legacy('special', $icon, loremIpsum()))
+		;
+
+	return $slate;
 }
--- src/UI/examples/Layout/Page/Standard/z_auxilliary.php
+++ src/UI/examples/Layout/Page/Standard/z_auxilliary.php
@@ -9,23 +9,56 @@ function _initIliasForPreview()
 	chdir('../../../../../../');
 	require_once("Services/Init/classes/class.ilInitialisation.php");
 	require_once('src/UI/examples/MainControls/MetaBar/metabar.php');
-	require_once('src/UI/examples/MainControls/MainBar/mainbar.php');
-	require_once('src/UI/examples/Layout/Page/Standard/ui.php');
 	ilInitialisation::initILIAS();
 }
 
 
-function pagedemoContent($f)
+function demoBreadcrumbs($f)
+{
+	$crumbs = array (
+		$f->link()->standard("entry1", '#'),
+		$f->link()->standard("entry2", '#'),
+		$f->link()->standard("entry3", '#'),
+		$f->link()->standard("entry4", '#')
+	);
+	return $f->breadcrumbs($crumbs);
+}
+
+function demoContent($f)
 {
 	return array (
 		$f->panel()->standard('Demo Content',
-			$f->legacy("some content<br>some content<br>some content<br>x.")),
-		$f->panel()->standard('Demo Content 2',
-			$f->legacy("some content<br>some content<br>some content<br>x.")),
+			$f->legacy("some content<br>some content<br>some content")),
+		$f->panel()->standard('Demo Content 2',	$f->legacy(loremShort())),
 		$f->panel()->standard('Demo Content 3',
-			$f->legacy("some content<br>some content<br>some content<br>x.")),
-		$f->panel()->standard('Demo Content 4',
-			$f->legacy("some content<br>some content<br>some content<br>x."))
+			$f->legacy("some content<br>some content<br>some content")),
+		$f->panel()->standard('Demo Content 4',	$f->legacy(loremIpsum()))
 	);
 }
 
+function loremIpsum():string {
+	return <<<EOT
+	<h2>Lorem ipsum</h2>
+	<p>
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+	</p>
+	<p>
+	Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+	</p>
+	<p>
+	Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
+	</p>
+	<p>
+	Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer
+	</p>
+EOT;
+}
+
+function loremShort(): string {
+	return <<<EOT
+	<h2>Some Shorter Content</h2>
+	<p>
+	Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+	</p>
+EOT;
+}
